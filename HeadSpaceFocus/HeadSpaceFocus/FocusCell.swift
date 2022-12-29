d6e01d0eeedc26c6fd98b2c6105604a3220d3fb8
Yuhyeon <120432007+Yuhyeon@users.noreply.github.com>
2022-12-29 21:05:57 +0900

    Add files via upload

---
+++ HeadSpaceFocus/HeadSpaceFocus/FocusCell.swift
@@ -0,0 +1,26 @@
+//
+//  FocusCell.swift
+//  HeadSpaceFocus
+//
+//  Created by 김유현 on 2022/12/29.
+//
+
+import UIKit
+
+class FocusCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var thumbnailIamgeView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        contentView.backgroundColor = UIColor.systemIndigo
+        contentView.layer.cornerRadius = 10
+    }
+    func configure(_ item: Focus) {
+        titleLabel.text = item.title
+        descriptionLabel.text = item.description
+        thumbnailIamgeView.image = UIImage(systemName: item.imageName)?.withRenderingMode(.alwaysOriginal)
+    }
+}
